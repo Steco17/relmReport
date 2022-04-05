2505b9e90bb8e9a3be4b11642bf60f333ca7982c
Steco17 <moyo.stephane@gmail.com>
2022-04-05 10:49:11 +0000

    fix:clean and create migration

---
+++ database/migrations/2022_04_05_095213_create_coverages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoveragesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coverages', function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('report_id');
+            $table->text("building");
+            $table->text("call_nature");
+            $table->longText("follow_up");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coverages');
+    }
+}
